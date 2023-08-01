f7cf8761ec50213523b51606a3bfd08f4cf0a672
Rahul Das <99030792+Rahul-IND@users.noreply.github.com>
2023-08-01 18:05:47 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './keyExperts.html', './branch.html', './keyExperts.html'],
+  content: ['./index.html', './services.html', './about.html', './experts.html', './branch.html', './ocs.html', './links.html'],
   theme: {
     extend: {
       fontFamily: {
@@ -11,17 +11,17 @@ module.exports = {
       'blue': '#000028',
       'blue1': '#00193c',
       'blue2': '#002949',
-      'white': '#ffffff',
-      'textwhite': '#fff',
-      'cyan': '#00ffb9',
-      'cyan1': '#00cccc',
-      'gray': '#B2BEB5',
-      'black' : '#000'
-
+      'white' : '#ffffff',
+      'textwhite' : '#fff',
+      'cyan' : '#00ffb9',
+      'cyan1' : '#00cccc',
+      'gray' :'#B2BEB5',
+      
     }
   },
   plugins: [
     // ...
+    // require('@tailwindcss/aspect-ratio'),
   ],
 }
 
